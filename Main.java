806f5b1d4d71fbae36efde58ed6b37a03cf32af8
abylay06 <abylaia34@gmail.com>
2026-09-09 17:25:11 +0500

    Just testing my java skills

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Person person = new Person(18, "John");
+        System.out.println(person.toString());
+    }
+}
---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    private int age;
+    private String name;
+
+    public Person(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "name: " + name + ", age: " + age;
+    }
+}
